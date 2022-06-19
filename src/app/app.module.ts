f7de5e30cd881824268ffcd1fb524ded86810b53
Carlos del Fresno <cdelfresno@iti.es>
2022-06-19 10:01:07 +0200

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import { AppComponent } from './app.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { ListadoComponent } from './components/listado/listado.component';
 import { DetalleUsuarioComponent } from './components/detalle-usuario/detalle-usuario.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, BuscadorComponent, ListadoComponent, DetalleUsuarioComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    BuscadorComponent,
+    ListadoComponent,
+    DetalleUsuarioComponent,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
